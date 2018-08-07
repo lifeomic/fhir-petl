ac6acc7f43cd577cc961ad76971b22d5b495a39d
Anthony Roach <anthony.roach@lifeomic.com>
2018-08-07 13:16:18 -0400

    added medications and an icon

--- etl.py
+++ etl.py
@@ -1,18 +1,14 @@
-from collections import OrderedDict
 from datetime import timedelta
-import fhir
-from fhir import to_json, to_patient, to_procedure, to_condition, to_observation, resolve, mkdirp, number
 import petl as etl
-
-parse_year = etl.dateparser('%Y')
+from fhir import to_json, resolve, mkdirp, number, join, year
 
 def map_race(race):
     return {
-        'AMERICAN INDIAN AND ALASKA NATIVE': ('1002-5', 'American Indian or Alaska Native'),
-        'ASIAN': ('2028-9', 'Asian'),
-        'BLACK OR AFRICAN AMERICAN': ('2054-5', 'Black or African American'),
-        'HISPANIC OR LATINO': ('2106-3', 'White'),
-        'WHITE': ('2106-3', 'White'),
+        'AMERICAN INDIAN AND ALASKA NATIVE': ('http://hl7.org/fhir/v3/Race', '1002-5', 'American Indian or Alaska Native'),
+        'ASIAN': ('http://hl7.org/fhir/v3/Race', '2028-9', 'Asian'),
+        'BLACK OR AFRICAN AMERICAN': ('http://hl7.org/fhir/v3/Race', '2054-5', 'Black or African American'),
+        'HISPANIC OR LATINO': ('http://hl7.org/fhir/v3/Race', '2106-3', 'White'),
+        'WHITE': ('http://hl7.org/fhir/v3/Race', '2106-3', 'White'),
         'MULTIRACIAL': None
     }.get(race, None)
 
@@ -22,8 +18,8 @@ patients = (etl.io.csv.fromcsv(resolve('work/patients.csv'))
                 'subject_id': 'STUDYID',
                 'race': ('RACE', map_race),
                 'gender': ('SEX', {'F': 'female', 'M': 'male'}),
-                'birth_date': ('BIRTH_YR', parse_year),
-                'index_date': ('INDEX_YEAR', parse_year)
+                'birth_date': ('BIRTH_YR', year),
+                'index_date': ('INDEX_YEAR', year)
             }, True)
             .head(100))
 
@@ -49,7 +45,7 @@ conditions = (encounters
               .fieldmap({
                   'id': 'CONDITION_ID',
                   'onset': lambda rec: rec['index_date'] + timedelta(int(rec['DAYS_ADM_INDEX'])),
-                  'code': lambda rec: ('http://hl7.org/fhir/sid/icd-9-cm', rec['DX_CODE'], None),
+                  'code': lambda rec: ('http://hl7.org/fhir/sid/icd-9-cm', rec['DX_CODE']),
                   'subject': 'subject'
               }, True)
               .head(1000))
@@ -66,8 +62,47 @@ observations = (etl.io.csv.fromcsv(resolve('work/observations.csv'))
                 .select('value', lambda x: x)
                 .head(1000))
 
+def medications(rec):
+    return [
+        ('http://hl7.org/fhir/sid/ndc', rec['NDC_CODE'], rec['DRUG_NAME']),
+        ('urn:oid:2.16.840.1.113883.6.68', rec['GPI_CODE'], rec['DRUG_NAME'])
+    ]
+
+med_dispenses = (etl.io.csv.fromcsv(resolve('work/med_dispenses.csv'))
+                 .hashjoin(index, lkey='CASE_ID', rkey='subject_id')
+                 .fieldmap({
+                     'id': 'ID',
+                     'date': lambda rec: rec['index_date'] + timedelta(int(rec['DAYS_VIS_INDEX'])),
+                     'medication': medications,
+                     'quantity': ('DISPENSE_AMOUNT', number),
+                     'daysSupply': ('NUMBER_OF_DAYS_SUPPLY', number),
+                     'text': lambda rec: join(rec['DOSAGE_FORM_TEXT'], rec['DRUG_GROUP'], rec['DRUG_CLASS']),
+                     'subject': 'subject'
+                 }, True)
+                 .head(1000))
+
+def medications2(rec):
+    return [
+        ('http://hl7.org/fhir/sid/ndc', rec['NDC'], rec['ORDER_NAME']),
+        ('urn:oid:2.16.840.1.113883.6.68', rec['GPI'], rec['ORDER_NAME'])
+    ]
+
+med_requests = (etl.io.csv.fromcsv(resolve('work/med_requests.csv'))
+                .hashjoin(index, lkey='STUDYID', rkey='subject_id')
+                .fieldmap({
+                    'id': 'ID',
+                    'date': lambda rec: rec['index_date'] + timedelta(int(rec['DAYS_ORDER_INDEX'])),
+                    'medication': medications2,
+                    'text': lambda rec: join(rec['DRUG_GROUP'], rec['DRUG_CLASS']),
+                    'subject': 'subject'
+                }, True)
+                .head(1000))
+
+
 mkdirp(resolve('fhir'))
-to_json(patients, to_patient, resolve('fhir/patients.json'))
-to_json(procedures, to_procedure, resolve('fhir/procedures.json'))
-to_json(conditions, to_condition, resolve('fhir/conditions.json'))
-to_json(observations, to_observation, resolve('fhir/observations.json'))
+to_json(patients, 'Patient', resolve('fhir/patients.json'))
+to_json(procedures, 'Procedure', resolve('fhir/procedures.json'))
+to_json(conditions, 'Condition', resolve('fhir/conditions.json'))
+to_json(observations, 'Observation', resolve('fhir/observations.json'))
+to_json(med_dispenses, 'MedicationDispense', resolve('fhir/med_dispenses.json'))
+to_json(med_requests, 'MedicationRequest', resolve('fhir/med_requests.json'))
--- fhir.py
+++ fhir.py
@@ -4,11 +4,15 @@ from uuid import uuid4
 import os
 import sys
 
-def number(x):
-    try:
-        return int(x)
-    except ValueError:
-        return float(x)
+def join(*args):
+    result = ''
+    for arg in args:
+        if arg:
+            result += ' %s'%arg
+    return result.strip()
+
+number = etl.numparser()
+year = etl.dateparser('%Y')
 
 def mkdirp(path):
     path = resolve(path)
@@ -28,32 +32,40 @@ def preprocess(table, source, sort=None, ids=['ID'], convert=int):
         table = table.sort(sort, buffersize=1000000)
     table.tocsv(source)
 
-def to_json(table, mapper, source):
+def to_json(table, resourceType, source):
+    mapper = types[resourceType]
     table = table.fieldmap({'data': mapper}, True)
     etl.io.text.totext(table, source, 'utf8', template='{data}\n')
     return table
 
-def tuple_to_codeable_concept(x, defaultSystem=None):
+def to_codeable_concept(x):
+    if type(x) is list:
+        result = map(tuple_to_code, x)
+    else:
+        result = [tuple_to_code(x)]
+    return {'coding': [c for c in result if c.get('code')]}
+
+def tuple_to_code(x):
     if len(x) == 2:
-        system = defaultSystem
-        code, display = x
+        display = None
+        system, code = x
     else:
         system, code, display = x
 
     if system and code and display:
-        return {'coding': [{'system': system, 'code': code, 'display': display}]}
+        return {'system': system, 'code': code, 'display': display}
     elif code and display:
-        return {'coding': [{'code': code, 'display': display}]}
+        return {'code': code, 'display': display}
     elif system and code:
-        return {'coding': [{'system': system, 'code': code}]}
+        return {'system': system, 'code': code}
     elif system and display:
-        return {'coding': [{'system': system, 'display': display}]}
+        return {'system': system, 'display': display}
     elif code:
-        return {'coding': [{'code': code}]}
+        return {'code': code}
     elif display:
-        return {'coding': [{'display': display}]}
+        return {'display': display}
     elif system:
-        return {'coding': [{'system': system}]}
+        return {'system': system}
 
 def to_patient(rec):
     result = {}
@@ -62,7 +74,7 @@ def to_patient(rec):
     if rec['subject_id']:
         result['identifier'] = [
             {
-                'type': tuple_to_codeable_concept(('http://hl7.org/fhir/v2/0203', 'ANON')),
+                'type': to_codeable_concept(('http://hl7.org/fhir/v2/0203', 'ANON')),
                 'system': 'http://lifeomic.com/fhir/subject-id',
                 'value': rec['subject_id']
             }
@@ -71,7 +83,7 @@ def to_patient(rec):
         result['extension'] = [
             {
                 'url': 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-race',
-                'valueCodeableConcept': tuple_to_codeable_concept(rec['race'], 'http://hl7.org/fhir/v3/Race')
+                'valueCodeableConcept': to_codeable_concept(rec['race'])
             }
         ]
     if rec['gender']:
@@ -87,7 +99,7 @@ def to_procedure(rec):
     if rec['date']:
         result['performedDateTime'] = rec['date'].isoformat()
     if rec['code']:
-        result['code'] = tuple_to_codeable_concept(rec['code'])
+        result['code'] = to_codeable_concept(rec['code'])
     if rec['subject']:
         result['subject'] = {'reference': 'Patient/' + rec['subject']}
     return json.dumps(result)
@@ -99,7 +111,7 @@ def to_condition(rec):
     if rec['onset']:
         result['onsetDateTime'] = rec['onset'].isoformat()
     if rec['code']:
-        result['code'] = tuple_to_codeable_concept(rec['code'])
+        result['code'] = to_codeable_concept(rec['code'])
     if rec['subject']:
         result['subject'] = {'reference': 'Patient/' + rec['subject']}
     return json.dumps(result)
@@ -111,7 +123,7 @@ def to_observation(rec):
     if rec['date']:
         result['effectiveDateTime'] = rec['date'].isoformat()
     if rec['code']:
-        result['code'] = tuple_to_codeable_concept(rec['code'])
+        result['code'] = to_codeable_concept(rec['code'])
     if rec['subject']:
         result['subject'] = {'reference': 'Patient/' + rec['subject']}
     if rec['value']:
@@ -121,3 +133,44 @@ def to_observation(rec):
         else:
             result['valueString'] = value
     return json.dumps(result)
+
+def to_med_dispense(rec):
+    result = {}
+    result['id'] = rec['id']
+    result['resourceType'] = 'MedicationDispense'
+    if rec['date']:
+        result['whenHandedOver'] = rec['date'].isoformat()
+    if rec['medication']:
+        result['medicationCodeableConcept'] = to_codeable_concept(rec['medication'])
+    if rec['subject']:
+        result['subject'] = {'reference': 'Patient/' + rec['subject']}
+    if rec['quantity']:
+        result['quantity'] = {'value': rec['quantity']}
+    if rec['daysSupply']:
+        result['daysSupply'] = {'value': rec['quantity'], 'unit': 'days'}
+    if rec['text']:
+        result['text'] = {'div': rec['text']}
+    return json.dumps(result)
+
+def to_med_request(rec):
+    result = {}
+    result['id'] = rec['id']
+    result['resourceType'] = 'MedicationRequest'
+    if rec['date']:
+        result['date'] = rec['date'].isoformat()
+    if rec['medication']:
+        result['medicationCodeableConcept'] = to_codeable_concept(rec['medication'])
+    if rec['subject']:
+        result['subject'] = {'reference': 'Patient/' + rec['subject']}
+    if rec['text']:
+        result['text'] = {'div': rec['text']}
+    return json.dumps(result)
+
+types = {
+    'Procedure': to_procedure,
+    'Patient': to_patient,
+    'Condition': to_condition,
+    'Observation': to_observation,
+    'MedicationDispense': to_med_dispense,
+    'MedicationRequest': to_med_request
+}
